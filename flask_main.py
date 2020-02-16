f6e6ddccc45b7b9ae6317c2a059b54aebb8bfe95
Paul Rosen <prosen@usf.edu>
2020-02-15 21:27:25 -0500

    renaming files

--- flask_main.py
+++ flask_main.py
@@ -5,17 +5,16 @@ from flask import request
 from flask import send_file
 from flask import send_from_directory
 
-import common
-
+import experiments
 import webbrowser
 
 app = Flask(__name__)
 
-datasets = common.get_datasets()
+datasets = experiments.get_datasets()
 
 for _ds in datasets:
     for _df in datasets[_ds]:
-        common.generate_metric_data(_ds, _df)
+        experiments.generate_metric_data(_ds, _df)
 
 # webbrowser.open_new_tab("http://localhost:6500")
 
@@ -52,22 +51,22 @@ def get_metric_data():
     ds = request.args.get("dataset")
     df = request.args.get("datafile")
 
-    if not common.valid_dataset(datasets, ds, df):
+    if not experiments.valid_dataset(datasets, ds, df):
         print("unknown dataset: " + ds + " or data file: " + df)
         return "{}"
 
-    metric_data = common.generate_metric_data(ds, df)
-    metric_reg = [common.metric_regression(metric_data, 'approx entropy', 'L1 norm'),
-                  common.metric_regression(metric_data, 'approx entropy', 'L_inf norm'),
-                  common.metric_regression(metric_data, 'approx entropy', 'peak wasserstein'),
-                  common.metric_regression(metric_data, 'approx entropy', 'peak bottleneck')]
+    metric_data = experiments.generate_metric_data(ds, df)
+    metric_reg = [experiments.metric_regression(metric_data, 'approx entropy', 'L1 norm'),
+                  experiments.metric_regression(metric_data, 'approx entropy', 'L_inf norm'),
+                  experiments.metric_regression(metric_data, 'approx entropy', 'peak wasserstein'),
+                  experiments.metric_regression(metric_data, 'approx entropy', 'peak bottleneck')]
 
     return json.dumps({'metric': metric_data, 'rank': metric_reg})
 
 
 @app.route('/all_ranks', methods=['GET', 'POST'])
 def get_all_rank_data():
-    return json.dumps( common.metric_ranks(datasets) )
+    return json.dumps( experiments.metric_ranks(datasets) )
 
 
 @app.route('/data', methods=['GET', 'POST'])
@@ -75,11 +74,11 @@ def get_data():
     ds = request.args.get("dataset")
     df = request.args.get("datafile")
 
-    if not common.valid_dataset(datasets, ds, df):
+    if not experiments.valid_dataset(datasets, ds, df):
         print("unknown dataset: " + ds + " or data file: " + df)
         return "{}"
 
-    input_signal = common.load_dataset(ds, df)
-    res = common.process_smoothing(input_signal, request.args.get("filter"), float(request.args.get("level")))
+    input_signal = experiments.load_dataset(ds, df)
+    res = experiments.process_smoothing(input_signal, request.args.get("filter"), float(request.args.get("level")))
 
     return json.dumps(res)
