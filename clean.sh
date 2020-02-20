4b4d3e7cdd64e2243a24fcccd6e2b06e859f0f95
Paul Rosen <prosen@usf.edu>
2020-02-19 23:38:08 -0500

    split out cleanup code

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Delete old files and directories
+rm -rf __pycache__
+rm -rf lcsmooth/__pycache__
+rm -rf topolines/__pycache__
+rm -rf venv
+rm -rf entropy
+rm tmp_*.pd
--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Delete old files and directories
-rm -rf __pycache__
-rm -rf lcsmooth/__pycache__
-rm -rf topolines/__pycache__
-rm -rf venv
-rm -rf entropy
-rm tmp_*.pd
+# Remove old files
+./clean.sh
 
 # Create Virtual Environment
 python3 -m venv venv
